3e880419f1d1d2b2c7d0c2940e4ea29274516db5
Aidil <refandyakbar1709@gmail.com>
2026-08-01 09:20:05 +0700

    update dikit

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,6 @@ Route::get('/dashboard', function ()
     return view('dashboard');
 })->middleware('auth')->name('dashboard');
 
-
-Route::middleware('auth')
-    ->group(function () 
-    {
-        Route::get('/profile', [ProfileController::class, 'edit'])
-            ->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])
-            ->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])
-            ->name('profile.destroy');
-    }
-);
-
 Route::middleware('auth')
     ->prefix('admin') // Mengatur URL di browser agar berawalan .../admin/...
     ->name('admin.') // Mengatur Nama Route di kode agar berawalan admin.

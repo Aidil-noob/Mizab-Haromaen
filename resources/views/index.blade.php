f0e7ccf063c66d7f7021edc9ea8590b11be3021c
Aidil-noob <refandyakbar1709@gmail.com>
2026-08-28 03:58:45 +0700

    perbaiki performa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,19 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mizab Haromaen</title>
-    <link rel="preconnect" href="https://www.chatbase.co" />
-    <link rel="dns-prefetch" href="https://www.chatbase.co" />
-    
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DFWYEQP8TG"></script>
+    <link rel="preconnect" href="https://www.chatbase.co" crossorigin/>
+    <link rel="dns-prefetch" href="https://www.chatbase.co"/>
+
     <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-DFWYEQP8TG');
+        (function () {
+            if (!window.chatbase || window.chatbase("getState") !== "initialized") {
+                window.chatbase = (...arguments) => {
+                    if (!window.chatbase.q) {
+                        window.chatbase.q = []
+                    }
+                    window.chatbase.q.push(arguments)
+                };
+                window.chatbase = new Proxy(window.chatbase, {
+                    get(target,prop) {
+                        if (prop === "q") {
+                            return target.q
+                        }
+                        return (...args) => target(prop,...args)
+                    }
+                })
+            }
+            const onLoad = function () {
+                const script = document.createElement("script");
+                script.src = "https://www.chatbase.co/embed.min.js";
+                script.id = "0hw2RyNGg918tAytZACN3";
+                script.domain = "www.chatbase.co";
+                document.body.appendChild(script)
+            };
+            if (document.readyState === "complete") {
+                onLoad()
+            } else {
+                window.addEventListener("load",onLoad)
+            }
+        }) ();
     </script>
-
     {{-- impor file css & js --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -408,38 +430,6 @@
             </div>
         </footer>
     </div>
-    <script async>
-        (function () {
-            if (!window.chatbase || window.chatbase("getState") !== "initialized") {
-                window.chatbase = (...arguments) => {
-                    if (!window.chatbase.q) {
-                        window.chatbase.q = []
-                    }
-                    window.chatbase.q.push(arguments)
-                };
-                window.chatbase = new Proxy(window.chatbase, {
-                    get(target,prop) {
-                        if (prop === "q") {
-                            return target.q
-                        }
-                        return (...args) => target(prop,...args)
-                    }
-                })
-            }
-            const onLoad = function () {
-                const script = document.createElement("script");
-                script.src = "https://www.chatbase.co/embed.min.js";
-                script.id = "0hw2RyNGg918tAytZACN3";
-                script.domain = "www.chatbase.co";
-                document.body.appendChild(script)
-            };
-            if (document.readyState === "complete") {
-                onLoad()
-            } else {
-                window.addEventListener("load",onLoad)
-            }
-        }) ();
-    </script>
     <script>
         function salin() {
             let teks = document.getElementById('bsi').value; //ambil tombol

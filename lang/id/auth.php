075ae918e6e8d8395870d180f2f8be43288ec220
Aidil-noob <refandyakbar1709@gmail.com>
2026-09-03 09:59:34 +0700

    tambah validasi error bahasa indonesia

---
+++ lang/id/auth.php
@@ -0,0 +1,7 @@
+<?php 
+
+return [
+    'failed' => 'Email atau Password salah.',
+    'password' => 'Password salah.',
+    'throttle' => 'Terlalu banyak percobaan, Coba lagi dalam :seconds detik.'
+];
